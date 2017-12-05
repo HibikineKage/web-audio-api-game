87fa2d87eb894fb39b061d7ab9df57dca3ed1a89
Hibikine Kage <goodsmile314159@gmail.com>
2017-12-05 12:20:13 +0900

    first commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,17 @@
+import path from 'path';
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'index.js',
+  }
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    compress: true,
+    port: 9000,
+    hot: true,
+  }
+};
+
+export default config;
