28755cbec2152cb3fdd1d3b669ca290c4cdc01b2
Hibikine Kage <goodsmile314159@gmail.com>
2017-12-06 11:39:01 +0900

    整合性を取るようにした

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,7 +5,7 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'index.js',
-  }
+  },
   devServer: {
     contentBase: path.join(__dirname, "public"),
     compress: true,
